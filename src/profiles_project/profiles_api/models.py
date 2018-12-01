a987be9e547801c9634a652c8d6c6fb4b1f890ef
dims337 <dims337@gmail.com>
2018-12-01 17:00:10 -0500

    fixed all bugs, indent errors, and completed migration

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -13,7 +13,7 @@ def create_user(self, email, name, password=None):
     if not email:       #check if email is not blank
         raise ValueError('user must have an email address')
 
-        email = self.normalize_email(email) #standardizes email
+        email = self.normalize_email(email) #standardizes/normalize email
         user = self.model(email=email, name=name)
 
         user.set_password(password) #set psswd which will be encrypted
@@ -25,34 +25,30 @@ def create_superuser(self, email, name, password):
     """creates and saves a new superuser with given details"""
         #function that tells django how to create superusers
 
-        user = self.create_user(email, name, password)
+    user = self.create_user(email, name, password)
 
+    user.is_superuser = True
+    user.is_staff = True
 
-        user.is_superuser = True
-        user.is_staff = True
-
-        user.save(using=self._db)
-
-
+    user.save(using=self._db)
 
 class Userprofile(AbstractBaseUser, PermissionsMixin):
-"""represent a user profile inside our system."""
+    """represent a user profile inside our system."""
 
-email = models.EmailField(max_length=255, unique=True)
-name = models.charField(max_length=255)
-is_active = models.BooleanField(default=True)
-is_staff = models.BooleanField(default=False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-objects = UserProfileManager() #object manager to help manage class
+    objects = UserProfileManager() #object manager to help manage class
 
-USERNAME_FIELD = 'email'
-REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
 def get_full_name(self):
     """used to get a users full name"""
 
     return self.name
-
 
 def get_short_name(self):
     """used to get short name"""
